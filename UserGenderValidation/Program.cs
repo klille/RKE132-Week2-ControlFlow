e0c6a95a7365a5e316f5d48497ec8c2eb0afa4bb
ees-ja perekonnanimi <klille@tktk.eel~>
2023-02-04 16:09:54 +0200

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajalt alida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select you gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+Console.WriteLine("Please, enter your last name:") ;
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
